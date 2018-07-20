e14d2e6ce4789b6efc14a0523b01ed0215fa8f8a
nigma1985 <nigma-mania@web.de>
2018-07-20 08:48:29 +0200

    decision making in readPi

---
+++ module/decision.py
@@ -0,0 +1,45 @@
+from random import random
+from module.getOptions import findItm
+
+def decision(onSwitch = None, offSwitch = None, numChance = None, capChance = None):
+    if onSwitch is None:
+        onSwitch = ["ALLON"]
+    if offSwitch is None:
+        offSwitch = ["ALLOFF"]
+
+    def _item(i):
+        # if bool, take as is
+        # if str use getOptions
+        # if None then None
+        if type(i) == bool or i == None:
+            return i
+        elif type(i) == str:
+            return findItm(item = i)
+        else:
+            return None
+
+    def _list(_input):
+        items = []
+
+        # if list then
+        ## for all items in list check items
+        ## otherwise check single item
+        if type(_input) == list:
+            for _in in _input:
+                items.append(_item(i = _in))
+        else:
+            items.append(_item(_input))
+
+        return items
+
+    on = _list(_input = onSwitch)
+    off = _list(_input = offSwitch)
+
+    if any(off):
+        return False
+    elif any(on):
+        return True
+    elif (numChance is None) or (numChance - capChance == 0):
+        return True
+    else:
+        random() <= 1 / (numChance - capChance)
--- tst.py
+++ tst.py
@@ -1,14 +1,26 @@
 import module.config as ini ## https://wiki.python.org/moin/ConfigParserExamples
 import module.getOptions as opt
 #import module.freyr.csvBuffer as csv
-import os
+import module.decision as dec
+import os, sys
 
-_input = [0, 'nix', 'out/FREYR_2020-20-00_2599_byangoma.csv', 'test', 'ALLON', 999, 666]
-print _input
+# _input = sys.argv
+# #_input = [0, 'nix', 'out/FREYR_2020-20-00_2599_byangoma.csv', 'test', 'ALLON', 999, 666]
+#
+# print _input
+# csv_name = opt.csvName(options = _input)
+#
+# all_on = opt.findItm("ALLON")
+# all_off = opt.findItm("ALLOFF")
+#
+# print all_on, all_off, csv_name
+#
+# print any(["True", False, None])
+#
+# print any(True)
+# print any(None)
+# print any("TXT")
 
-csv_name = opt.csvName(options = _input)
+#print dec.decision([False, False, False, False], None, 90, 90)
 
-all_on = opt.findItm("ALLON")
-all_off = opt.findItm("ALLOFF")
-
-print all_on, all_off, csv_name
+print dec.decision()
