72d012f2840cf9907d89fff3af036785bfac711a
nigma1985 <nigma-mania@web.de>
2018-07-24 08:19:43 +0200

    added testing

--- tst.py
+++ tst.py
@@ -1,26 +1,13 @@
 import module.config as ini ## https://wiki.python.org/moin/ConfigParserExamples
-import module.getOptions as opt
-#import module.freyr.csvBuffer as csv
 import module.decision as dec
-import os, sys
+import module.getOptions as opt
+import module.tools as tls
+import module.netTool as ntt
+import module.freyr.csvBuffer as bfr
+import module.read.pi as rpi
+import os
 
-# _input = sys.argv
-# #_input = [0, 'nix', 'out/FREYR_2020-20-00_2599_byangoma.csv', 'test', 'ALLON', 999, 666]
-#
-# print _input
-# csv_name = opt.csvName(options = _input)
-#
-# all_on = opt.findItm("ALLON")
-# all_off = opt.findItm("ALLOFF")
-#
-# print all_on, all_off, csv_name
-#
-# print any(["True", False, None])
-#
-# print any(True)
-# print any(None)
-# print any("TXT")
-
-#print dec.decision([False, False, False, False], None, 90, 90)
-
-print dec.decision()
+print "CPU TMP", tls.mean([rpi.getCPUtemperature(), rpi.get_cpu_temperature()])
+print "CPU %", rpi.cpu_percent()
+print "NOW", tls.now()
+print "UTC", tls.utcnow() 
