9c82f84830267276284b417189eda3a0f8a93881
nigma1985 <nigma-mania@web.de>
2018-08-02 07:51:46 +0200

    testing csvName

--- tst.py
+++ tst.py
@@ -9,29 +9,5 @@ import module.freyr.csvBuffer as bfr
 import os
 
 import time
-#
-# now1 = ttl.now()
-# utc1 = ttl.utcnow()
-# nowsecs = ttl.mktime(now1)
-# #nowsecs = time.mktime(now1.timetuple())
-#
-# # cpu_tempA = rpi.getCPUtemperature()
-# # print "CPU %", rpi.cpu_percent()
-# #
-# # ram = rpi.virtual_memory()
-# # print "RAM", ram.total / 2**20       # MiB.
-# # print "RAM%", ram.percent
-# #
-# # disk = rpi.disk_usage('/')
-# # print "Disk", disk.total / 2**30     # GiB.
-# # print "Disk%", disk.percent
-# #
-# # cpu_tempB = rpi.get_cpu_temperature()
-# # print "CPU TMP", mean([float(cpu_tempA), cpu_tempB])
-#
-# utc2 = ttl.utcnow()
-# print "OffSet", str(ttl.roundTime(now1,roundTo=30*60) - ttl.roundTime(utc1,roundTo=30*60))
-# duration = (utc2-utc1)
-# print "duration", (float(duration.microseconds) / 10**6) + duration.seconds + (((duration.days * 24) * 60) * 60)
 
-print bfr.writeRow()
+bfr.csvName()
