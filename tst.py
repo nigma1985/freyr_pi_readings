75aa7f1848e439ba78dcb9c4fffb5901b90177d7
nigma1985 <nigma-mania@web.de>
2018-08-03 07:53:15 +0200

    timeTools testing

--- tst.py
+++ tst.py
@@ -2,7 +2,7 @@ import module.config as ini ## https://wiki.python.org/moin/ConfigParserExamples
 import module.decision as dec
 import module.getOptions as opt
 from module.tools import mean
-import module.timeTool as ttl
+import module.timeTools as ttl
 import module.netTool as ntt
 import module.freyr.csvBuffer as bfr
 #import module.read.pi as rpi
