3c2b7920f7f7b674be2725dbd3f14056b1ff222b
nigma1985 <nigma-mania@web.de>
2018-08-06 19:49:12 +0200

    documentation added

--- module/freyr/__init__.py
+++ module/freyr/__init__.py
@@ -12,7 +12,8 @@ def findConfig(sysOptions = sys.argv, sysKey = None, readVar = None, confSection
         sysConfig = sysConfig[-(len(sysConfig)-(len(sysKey)+1)):]
         return cleanType(sysConfig)
     elif readVar is not None:
+        ## if no argument has been passed a value from the script can be passed
         return readVar
-    ## if no argument has been passed, the config.ini is read
     else:
+        ## if no argument or value has been passed, the config.ini is read
         return ConfigSectionMapAdv(section = confSection, option = confOption, iniConfig = confFile)
