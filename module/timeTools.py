84bb8c53cc65ee595028e92d16d8cac9912300f1
nigma1985 <nigma-mania@web.de>
2018-08-07 08:30:33 +0200

    bug fix start() and end()

--- module/timeTools.py
+++ module/timeTools.py
@@ -33,22 +33,13 @@ def roundTime(dt=None, roundTo=60):
    return dt + timedelta(0,rounding-seconds,-dt.microsecond)
 
 def start():
-    now = now()
-    return now, utcnow(), mktime(now1)
-
-
-
-
-
-#utc2 = ttl.utcnow()
-#offset_utc = str(ttl.roundTime(now1,roundTo=30*60) - ttl.roundTime(utc1,roundTo=30*60))
-#duration = (utc2-utc1)
-#duration2 = (float(duration.microseconds) / 10**6) + duration.seconds + (((duration.days * 24) * 60) * 60)
+    now = datetime.now()
+    return now, datetime.utcnow(), mktime(now)
 
 def end(utc1):
     if utc1 is None:
         startBuffer = start()
         utc1 = startBuffer[1]
-    utc2 = utcnow()
+    utc2 = datetime.utcnow()
     duration = (utc2-utc1)
     return utc2, str(roundTime(now1,roundTo=30*60) - roundTime(utc1,roundTo=30*60)), duration, (float(duration.microseconds) / 10**6) + duration.seconds + (((duration.days * 24) * 60) * 60)
