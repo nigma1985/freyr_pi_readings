faf57038f818f4483e740752b96a79c9e217af14
nigma1985 <nigma-mania@web.de>
2018-08-14 17:37:23 +0200

    creating list from string and cleaning spaces

--- tst.py
+++ tst.py
@@ -5,3 +5,14 @@ print ntt.ping_singlehost("0.0.0.0", 5)
 
 print ntt.ping_host()
 print ntt.ping_host(hosts = ["0.0.0.0", "1.1.1.1"])
+
+
+x = "139.130.4.5,    ferienhauskeck.de,google.com,8.8.4.4  ,8.8.8.8,0.0.0.0, wikipedia.org ,heise.de,raspberrypi.org"
+x = x.split(",")
+y = None                                                                                                                 
+while y != x:
+    y = x                                                                                                                
+    x = [i.replace("  "," ") for i in x]
+x = [i.strip() for i in x]                                                                                               
+
+print ntt.ping_host(hosts = x)
