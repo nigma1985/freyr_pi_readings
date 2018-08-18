1540c3c541e988d8adb25e5a882c786ffc21c40a
nigma1985 <nigma-mania@web.de>
2018-08-18 18:29:40 +0200

    testing python3 read.pi

--- tst.py
+++ tst.py
@@ -1,26 +1,24 @@
 from module import *
 import module.netTools as ntt
+from module.read.pi import *
 
+#str = "139.130.4.5,    ferienhauskeck.de,google.com,8.8.4.4  ,8.8.8.8,0.0.0.0, wikipedia.org ,heise.de,raspberrypi.org"
+#print(ntt.ping_host(hosts = cleanList(string = str)))
 
-# print ntt.ping_singlehost()
-# print ntt.ping_singlehost("0.0.0.0", 5)
-#
-# print ntt.ping_host()
-# print ntt.ping_host(hosts = ["0.0.0.0", "1.1.1.1"])
-#
-#
+x = getCPUtemperature()
+print "getCPUtemperature", x, type(x)
 
-str = "139.130.4.5,    ferienhauskeck.de,google.com,8.8.4.4  ,8.8.8.8,0.0.0.0, wikipedia.org ,heise.de,raspberrypi.org"
-# x = x.split(",")
-# y = None
-# while y != x:
-#     y = x
-#     x = [i.replace("  "," ") for i in x]
-# x = [i.strip() for i in x]
+x = get_cpu_temperature()
+print "get_cpu_temperature", x, type(x)
 
-# print "cleanUnicode", cleanUnicode(var = "u'\u2103'")
-# print "str2list", str2list(var = str, symbol = ".")
-# print "cleanSpaces", cleanSpaces(var = "  N o   n e  ll ll me      ")
-# print "cleanType", cleanType(var = "10")
-# print "cleanList", cleanList(string = str)
-print(ntt.ping_host(hosts = cleanList(string = str)))
+x = getRAMinfo()
+print "getRAMinfo", x, type(x)
+
+x = cpu_percent()
+print "cpu_percent", x, type(x)
+
+x = virtual_memory()
+print "virtual_memory", x, type(x)
+
+x = disk_usage()
+print "disk_usage", x, type(x)
